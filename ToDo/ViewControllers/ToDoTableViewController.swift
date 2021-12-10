88f2e9e1ce0c97cfb33fda72ec93d1f29b36ca36
Edgar <engineeredkar@gmail.com>
2021-12-11 01:47:20 +0400

    create new tableviewcontroller for add new todo create left and right nav bar button items

--- ToDo/ViewControllers/ToDoTableViewController.swift
+++ ToDo/ViewControllers/ToDoTableViewController.swift
@@ -18,6 +18,7 @@ class ToDoTableViewController: UITableViewController {
         } else {
             todos = ToDo.loadSimpleToDos()
         }
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 
    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,6 +32,17 @@ class ToDoTableViewController: UITableViewController {
         let todo = todos[indexPath.row]
         cell.textLabel?.text = todo.title
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
     
 
